fbd052e79a466fdeceb257b53923daebc3fe707f
RpRun <82530937+RpRun@users.noreply.github.com>
2021-11-01 20:14:04 +0100

    type module added for index.js

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 import { createCard } from './utils.js'
 
+
 fetch('data.json')
     .then(data => data.json())
-    .then(reponse => {
+    .then(reponse => { 
 
         console.log(reponse);
 
-        const sectionDom = document.querySelector(`.photographers`)
+        const sectionDom = document.querySelector('.photographers')
         const photographers = reponse.photographers
 
-        for (let i = 0; i < photographers.length;i++) {
+        for (let i = 0; i < photographers.length; i++) {            
             const card = createCard(photographers[i])
             sectionDom.innerHTML += card
         }
--- utils.js
+++ utils.js
@@ -5,23 +5,23 @@ export const createCard = photographe => {
         
     });
 
-const card = `<div class="photographers__cards">
-<a href="./view/portfolio.html?photographerId=${photographe.id}" class="links__portfolio">
-    <img class="photographers__portrait"
-        src="./public/Photos/PhotographersIdPhotos/${photographe.portrait}" alt="">
-    <h2 class="photographers__name">${photographe.name}</h2>
-</a>
-<div class="photographers__infos">
-    <p class="localisation">${photographe.city}</p>
-    <p class="mojo">${photographe.tagline}</p>
-    <p class="cost">${photographe.price}</p>
-</div>
-<div class="filters" role="navigation" aria-label="tags filters">
-    <ul class="tags" >
-    <ul>${tagList}</ul>
-    </ul>
-</div>
-</div>`
+    const card = `<div class="photographers__cards">
+    <a href="./view/portfolio.html?photographerId=${photographe.id}" class="links__portfolio">
+        <img class="photographers__portrait"
+            src="./public/Photos/PhotographersIdPhotos/${photographe.portrait}" alt="">
+        <h2 class="photographers__name">${photographe.name}</h2>
+    </a>
+    <div class="photographers__infos">
+        <p class="localisation">${photographe.city}</p>
+        <p class="mojo">${photographe.tagline}</p>
+        <p class="cost">${photographe.price}</p>
+    </div>
+    <div class="filters" role="navigation" aria-label="tags filters">
+        <ul class="tags" >
+        <ul>${tagList}</ul>
+        </ul>
+    </div>
+    </div>`
 
 return card
 
