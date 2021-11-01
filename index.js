5c2eb1e939376ff11579023528032ae19fc23949
RpRun <82530937+RpRun@users.noreply.github.com>
2021-11-01 20:00:31 +0100

    index and utils.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+import { createCard } from './utils.js'
 
 fetch('data.json')
     .then(data => data.json())
@@ -6,7 +6,12 @@ fetch('data.json')
 
         console.log(reponse);
 
-        
+        const sectionDom = document.querySelector(`.photographers`)
+        const photographers = reponse.photographers
 
+        for (let i = 0; i < photographers.length;i++) {
+            const card = createCard(photographers[i])
+            sectionDom.innerHTML += card
+        }
 
     })
