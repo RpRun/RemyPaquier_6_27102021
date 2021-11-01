f0c89ba6c8a38a4807460a2fc5d78af3cb42d8df
RpRun <82530937+RpRun@users.noreply.github.com>
2021-11-01 19:13:43 +0100

    fetch data.json

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+fetch('data.json')
+    .then(data => data.json())
+    .then(reponse => {
+
+        console.log(reponse);
+
+        
+
+
+    })
---
+++ utils.js
@@ -0,0 +1,11 @@
+// export const createCard = photographe => {
+//     let tagList = ''
+//     photographe.tags.forEach(tag => { 
+//         tagList += `<li><a href="">#${tag}</a></li>`
+        
+//     });
+
+
+
+
+// }
