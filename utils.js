09e5d25648df5dc1c1d73c4d3b76c9be0665a405
RpRun <82530937+RpRun@users.noreply.github.com>
2021-11-02 18:42:40 +0100

    js tests

--- utils.js
+++ utils.js
@@ -25,49 +25,29 @@ return card
 
 }
 
-export const createHeader = photographe => {
-    let tagList = ''
-    photographe.tags.forEach(tag => { 
-        tagList += `<li><a href="">#${tag}</a></li>`
+// export const createHeader = photographe => {
+//     let tagList = ''
+//     photographe.tags.forEach(tag => { 
+//         tagList += `<li><a href="">#${tag}</a></li>`
         
-    });
+//     });
 
-    const header = `<header class="header__portfolio">
-        <a class="home-page-link" href="../index.html">
-            <img class="logo" src="../public/icones/logo.png" alt="Fisheye Home page"></a>
-        <div class="header__portfolio--wrapper">
-            <div class="photographers__infos--portfolio">
-                <h1 class="photographers__name">${photographe.name}</h1>
-                <p class="localisation">${photographe.city}, ${photographe.country}</p>
-                <p class="mojo">${photographe.tagline}</p> 
-                    <ul role=navigation class="tags photographer__tags aria-label="tags filters"">
-                    ${tagList}
-                    </ul>                   
-                <button class="header-contact modal-btn">Contactez-moi</button>
-            </div>
+//     const header = `<div class="header__portfolio--wrapper">
+//                 <div class="photographers__infos--portfolio">
+//                     <h1 class="photographers__name">${photographe.name}</h1>
+//                     <p class="localisation">${photographe.city}, ${photographe.country}</p>
+//                     <p class="mojo">${photographe.tagline}</p> 
+//                         <ul role=navigation class="tags photographer__tags aria-label="tags filters"">
+//                         ${tagList}
+//                         </ul>                   
+//                     <button class="header-contact modal-btn">Contactez-moi</button>
+//                 </div>
 
-            <img class="photographers__portrait" src="../public/Photos/PhotographersIdPhotos/${photographe.portrait}"
-                alt="portrait du photographe">
-        </div>
-        <div class="dropdown-wrapper">
-            <span>Trier par </span>
-            <nav class="sortBy" aria-label="Trier par">
-                <ul id="menubar" role="menubar" aria-label="Trier par">
-                    <li><a role="menuitem" aria-haspopup="true" aria-expanded="false" href="#" tabindex="0">
-                            Popularité</a>
-                    </li>
-                    <li><a role="menuitem" aria-haspopup="true" aria-expanded="false" href="#" tabindex="-1">
-                            Date</a>
-                    </li>
-                    <li><a role="menuitem" aria-haspopup="true" aria-expanded="false" href="#" tabindex="-1">
-                            Titre</a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    </header>`
+//             <img class="photographers__portrait" src="../public/Photos/PhotographersIdPhotos/${photographe.portrait}"
+//                 alt="portrait du photographe">
+//             </div>`
 
-return header    
+// return header    
 
 
-}
+// }
--- view/portfolio.js
+++ view/portfolio.js
@@ -1,45 +1,49 @@
-import { createHeader } from '../utils.js'
+// import { createHeader } from '../utils.js'
 
 
-fetch('../data.json')
-    .then(data => data.json())
-    .then(reponse => { 
+// fetch('../data.json')
+//     .then(data => data.json())
+//     .then(reponse => { 
 
-        console.log(reponse);
+//         console.log(reponse);
 
-        const sectionDom = document.querySelector('header')
-        const photographers = reponse.photographers
+//         const sectionDom = document.querySelector('.header__portfolio--wrapper')
+//         const photographers = reponse.photographers
 
-        for (let i = 0; i < photographers.length; i++) {            
-            const header = createHeader(photographers[i])
-            sectionDom.innerHTML += header
-        }
-
-    })
-
-
-
-// const createHeader = async () => {
-//     const response = await fetch('../data.json');
-//     const json = await response.json();
-//     return json
-// }
-
-// const init = async() => {
-//     const data = await createHeader()
-//     const h1 = document.querySelector('.photographers__name')
-
-//     // RECUPERATION DES DONNEES A AFFICHER
-//     const queryString = window.location.search
-//     const urlParams = new URLSearchParams(queryString)
-//     const urlId = urlParams.get('id')
-
-//     data.photographers.forEach(photographer => {
-//         if (photographer.id == urlId) {
-//             h1.innerHTML = photographer.name
+//         for (let i = 0; i < photographers.length; i++) {            
+//             const header = createHeader(photographers[i])
+//             sectionDom.innerHTML += header
 //         }
-//     });
-    
-// }
 
-// init()
+    // })
+
+
+
+const createHeader = async () => {
+    const response = await fetch('../data.json');
+    const json = await response.json();
+    return json
+}
+
+const init = async() => {
+    const data = await createHeader()
+    const h1 = document.querySelector('.photographers__name')
+    const localisation = document.querySelector('.localisation')
+
+    // RECUPERATION DES DONNEES A AFFICHER
+    const queryString = window.location.search
+    const urlParams = new URLSearchParams(queryString)
+    const urlId = urlParams.get('id')
+
+    data.photographers.forEach(photographer => {
+        if (photographer.id == urlId) {
+            h1.innerHTML = photographer.name
+            localisation.innerHTML = photographer.city
+
+
+        }
+    });
+    
+}
+
+init()
