4575264fdf130b8f9acc4d7144aaf5a532b0ba3c
RpRun <82530937+RpRun@users.noreply.github.com>
2021-12-15 02:22:09 +0100

    alt modified on images

--- utils/utils.js
+++ utils/utils.js
@@ -52,7 +52,7 @@ export const createPreview = media => {
     const mediapreview = (media.image || media.video)
     if (mediapreview == media.image) {
         const preview__list = `<li class="thumbnails"><a href="../../assets/Photos/medias/${mediapreview}">
-        <img class="thumbnails__medias" src="../../assets/Photos/medias/${mediapreview}" alt="${media.title}"></a>
+        <img class="thumbnails__medias" src="../../assets/Photos/medias/${mediapreview}" alt="Photo prise le ${media.date} ayant pour titre '${media.title}' et comme thème '${media.tags}'"></a>
         <div class="thumbnails__infos">
             <h2 class="thumbnails__heading">${media.title}</h2>
              <div class="likes-count" data-filter="" data-like-count="${media.likes}"><span class="total">${media.likes}</span><i tabindex="0" class="far fa-heart heart"></i>              
