43ffaeedcee3b8421688dd0ba550d1cd9d2de3c2
RpRun <82530937+RpRun@users.noreply.github.com>
2021-12-23 20:04:13 +0100

    working on compatibility with firefox

--- utils/utils.js
+++ utils/utils.js
@@ -171,7 +171,7 @@ export const urlTagsHandleur = () => {
 
             // On recherche dans ses tags si clickedTag est présent
             for (let j = 0; j < cardTags.length; j++) {
-                const tags = cardTags[j].outerText;
+                const tags = cardTags[j].innerText;
                 // Si oui, on affiche les cards
                 
                 if (newUrlTags.toLowerCase() == tags.toLowerCase()) {
