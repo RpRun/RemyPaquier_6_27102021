666d2dc38f29b59a2d76aaae8efbcdadbb8f685e
RpRun <82530937+RpRun@users.noreply.github.com>
2021-12-26 12:54:27 +0100

    favicon uncommented

--- pages/home/index.js
+++ pages/home/index.js
@@ -16,6 +16,7 @@ if (localstorage) {
     }
 
     tagsHandler();
+    
    
 } else {
     fetch('./data/data.json')
@@ -32,6 +33,7 @@ if (localstorage) {
             }
             tagsHandler();
         })
+        
 }
 
 
